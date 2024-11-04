f48acf5cab98c577c3b53d299b19a692803478a3
Priyanshu4042 <156579165+Priyanshu4042@users.noreply.github.com>
2024-11-04 21:01:30 +0530

    Update prog1.c

--- lab9/prog1.c
+++ lab9/prog1.c
@@ -1,43 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Define the structure for a doubly linked circular list node
+
 struct Node {
     int data;
     struct Node* next;
     struct Node* prev;
 };
 
-// Function prototypes
+
 struct Node* createNode(int data);
 void insertIntoCircularList(struct Node** head, int newData);
 void deleteFromCircularList(struct Node** head, int key);
 void traverseCircularList(struct Node* head);
 
-// Function to create a new node
+
 struct Node* createNode(int data) {
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
     newNode->data = data;
-    newNode->next = newNode; // Point to itself
-    newNode->prev = newNode; // Point to itself
+    newNode->next = newNode; 
+    newNode->prev = newNode; 
     return newNode;
 }
 
-// Function to insert an element into a doubly linked circular list
+
 void insertIntoCircularList(struct Node** head, int newData) {
     struct Node* newNode = createNode(newData);
     if (*head == NULL) {
-        *head = newNode; // If the list is empty, set head to new node
+        *head = newNode; 
     } else {
-        struct Node* tail = (*head)->prev; // Get the last node
-        tail->next = newNode; // Link the last node to new node
-        newNode->prev = tail; // Link new node back to last node
+        struct Node* tail = (*head)->prev;
+        tail->next = newNode; 
+        newNode->prev = tail; 
     }
-    newNode->next = *head; // Link new node to head
-    (*head)->prev = newNode; // Link head back to new node
+    newNode->next = *head; 
+    (*head)->prev = newNode; 
 }
 
-// Function to delete an element from a doubly linked circular list
+
 void deleteFromCircularList(struct Node** head, int key) {
     if (*head == NULL) {
         printf("List is empty.\n");
@@ -47,17 +47,17 @@ void deleteFromCircularList(struct Node** head, int key) {
     struct Node* current = *head;
     do {
         if (current->data == key) {
-            if (current->next == current) { // Only one node in the list
+            if (current->next == current) {
                 free(current);
-                *head = NULL; // List becomes empty
+                *head = NULL;
                 return;
             } else {
                 struct Node* prevNode = current->prev;
                 struct Node* nextNode = current->next;
-                prevNode->next = nextNode; // Link previous node to next node
-                nextNode->prev = prevNode; // Link next node back to previous node
+                prevNode->next = nextNode; 
+                nextNode->prev = prevNode; 
                 if (current == *head) {
-                    *head = nextNode; // Update head if necessary
+                    *head = nextNode; 
                 }
                 free(current);
                 return;
@@ -69,7 +69,7 @@ void deleteFromCircularList(struct Node** head, int key) {
     printf("Element %d not found in the list.\n", key);
 }
 
-// Function to traverse the circular list
+
 void traverseCircularList(struct Node* head) {
     if (head == NULL) {
         printf("List is empty.\n");
@@ -83,7 +83,7 @@ void traverseCircularList(struct Node* head) {
     printf("\n");
 }
 
-// Main function
+
 int main() {
     struct Node* head = NULL;
     int choice, data;
